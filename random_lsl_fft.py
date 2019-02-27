03165eaf2a8f1ea6f52879d85269d9ff01133b6c
Corey Scheideman <corey9878@gmail.com>
2019-02-27 00:50:15 -0500

    prepare for sending BB8 commands based on O1 Power Threshold

--- random_lsl_fft.py
+++ random_lsl_fft.py
@@ -2,28 +2,20 @@
 LSL."""
 
 import time
-import numpy as np
 import scipy
 from scipy import signal
 from scipy.signal import butter, lfilter
-from pylsl import StreamInfo, StreamOutlet
-# from bb8 import BB8
+# from pylsl import StreamInfo, StreamOutlet
+from bb8 import BB8
 
-# from random import random
-
-# first create a new stream info (here we set the name to BioSemi,
-# the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
-# last value would be the serial number of the device or some other more or
-# less locally unique identifier for the stream as far as available (you
-# could also omit it but interrupted connections wouldn't auto-recover)
 stream_name = 'BioSemi'
 stream_type = 'EEG'
 num_channel = 14
 sample_freq = 128
 headset_uid = 'myuid34234'
-info = StreamInfo(stream_name, stream_type, num_channel,
-                  sample_freq, 'float32', headset_uid)
-outlet = StreamOutlet(info)
+# info = StreamInfo(stream_name, stream_type, num_channel,
+#                   sample_freq, 'float32', headset_uid)
+# outlet = StreamOutlet(info)
 
 eeg_bands = {'Delta': (0, 4),
              'Theta': (4, 8),
@@ -51,11 +43,14 @@ bp_low = 1.
 bp_high = 50.
 thresh_low = -100
 thresh_high = 100
-o_alpha_thresh = 50.
-# MAC_ADDR = 'F2:D8:37:4B:CE:F1'
-# bb = BB8(MAC_ADDR)
-# bb.cmd(0x02, 0x20, [0x10, 0x10, 0x10, 0])
-# bb.cmd(0x02, 0x21, [0xff])
+o_alpha_thresh = 10.
+
+MAC_ADDR = 'F2:D8:37:4B:CE:F1'
+bb = BB8(MAC_ADDR)
+bb.cmd(0x02, 0x21, [0xff])
+heading = 0
+red = [0xff, 0x00, 0x00, 0]
+blue = [0x00, 0xff, 0xff, 0]
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -80,62 +75,65 @@ def calc(x, fmin, fmax, fs=sample_freq):
     return y
 
 
-# def calc_bands_power(x, dt, bands):
-#     """
-#     From https://dsp.stackexchange.com/a/48210
-#     :param x: data array for one channel
-#     :param dt: sample frequency
-#     :param bands: dict of eeg_bands
-#     :return: dict of power values one for each band
-#     """
-#     f, psd = scipy.signal.welch(x, fs=1. / dt)
-#     power = {band: np.mean(psd[np.where((f >= lf) & (f <= hf))])
-#              for band, (lf, hf) in bands.items()}
-#     return power
+def roll(clockwise):
+    global bb
+    global heading
+    if clockwise:
+        heading += 15
+        direction = [100, (heading & 0xff00) >> 8, heading & 0xff, 1]
+    else:
+        heading -= 15
+        direction = [100, (heading & 0xff00) >> 8, heading & 0xff, 1]
+    while heading < 0:
+        heading += 360
+    while heading > 359:
+        heading -= 360
+    bb.cmd(0x02, 0x30, direction)
 
+
+def color(c):
+    global bb
+    bb.cmd(0x02, 0x20, c)
 
 data_file = open('trimmed_emotiv_values_2019-02-05_18-32-30.371051.csv', 'r')
-data_arr = np.asarray([data_file.next().strip().split(',')], dtype=np.float64)
-
-# print("now sending data...")
-
+data_arr = []
+i = 0
 for line in data_file:
 
-    packet_data = np.asarray(line.strip().split(','), dtype=np.float64)
+    data_arr.append(map(float, line.strip().split(',')))
 
-    if len(data_arr) == num_packets:
+    if len(data_arr) !=0 and len(data_arr) % num_packets == 0:
 
         # Get Data for O1 and O2 channel
-        o1_data = data_arr[:, chans['O1']]
-        o2_data = data_arr[:, chans['O2']]
+        o1_data = [col[chans['O1']] for col in data_arr]
+        o2_data = [col[chans['O2']] for col in data_arr]
 
         # Filtering
         # o1_data_filt = butter_bandpass_filter(o1_data, bp_low, bp_high, sample_freq, order=5)
         # o2_data_filt = butter_bandpass_filter(o2_data, bp_low, bp_high, sample_freq, order=5)
 
         # Thresholding
-        # o1_amplitude = np.max(o1_data) - np.min(o1_data)
-        # o2_amplitude = np.max(o2_data) - np.min(o2_data)
+        # o1_amplitude = max(o1_data[i-num_packets+1:i]) - min(o1_data[i-num_packets+1:i])
+        # o2_amplitude = max(o2_data[i-num_packets+1:i]) - min(o2_data[i-num_packets+1:i])
 
         # Calculate Alpha Band Power for O1 and O2
         fmin, fmax = eeg_bands['Alpha']
         p_o1 = calc(o1_data, fmin, fmax)
         p_o2 = calc(o2_data, fmin, fmax)
 
-        print 'New Packet Buffer [64]:'
-        print 'O1 Alpha Power:', p_o1
-        print 'O2 Alpha Power:', p_o2
+        # print 'O1 Alpha Power:', p_o1, '|', 'O2 Alpha Power:', p_o2
 
-        # Reset the data array
-        data_arr = np.asarray([packet_data])
+        if p_o1 > o_alpha_thresh:
+            print 'b.color(red)'
+            print 'b.roll(False)'
+        else:
+            print 'b.color(blue)'
+            print 'b.roll(True)'
 
         # now send it and wait for a bit
         # outlet.push_sample(power_values_delta)
         time.sleep(1.0 / sample_freq)
-    else:
-        # keep collecting pakets until 64
-        data_arr = np.append(data_arr, [packet_data], axis=0)
 
-print
+    i += 1
 
 data_file.close()
