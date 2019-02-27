e60e88cb78b9492b9bf5b32858f3d477fe6c6fb7
Corey Scheideman <corey9878@gmail.com>
2019-02-26 19:05:20 -0500

    Power value calculation working

--- random_lsl_fft.py
+++ random_lsl_fft.py
@@ -3,10 +3,13 @@ LSL."""
 
 import time
 import numpy as np
+import scipy
+from scipy import signal
+from scipy.signal import butter, lfilter
 from pylsl import StreamInfo, StreamOutlet
-from scipy.signal import welch
-import mne
-# from random import randint as rand
+# from bb8 import BB8
+
+# from random import random
 
 # first create a new stream info (here we set the name to BioSemi,
 # the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
@@ -43,96 +46,94 @@ chans = dict(F3=0,
              FC6=12,
              F4=13)
 
+num_packets = 64
 bp_low = 1.
 bp_high = 50.
+thresh_low = -100
+thresh_high = 100
+o_alpha_thresh = 50.
+# MAC_ADDR = 'F2:D8:37:4B:CE:F1'
+# bb = BB8(MAC_ADDR)
+# bb.cmd(0x02, 0x20, [0x10, 0x10, 0x10, 0])
+# bb.cmd(0x02, 0x21, [0xff])
 
 
-def calc_bands_power(x, dt, bands):
-    """
-    From https://dsp.stackexchange.com/a/48210
-    :param x: data array for one channel
-    :param dt: sample frequency
-    :param bands: dict of eeg_bands
-    :return: dict of power values one for each band
-    """
-    f, psd = welch(x, fs=1. / dt)
-    power = {band: np.mean(psd[np.where((f >= lf) & (f <= hf))])
-             for band, (lf, hf) in bands.items()}
-    return power
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+def calc(x, fmin, fmax, fs=sample_freq):
+    f, Pxx = scipy.signal.periodogram(x, fs=fs)
+    ind_min = scipy.argmax(f > fmin) - 1
+    ind_max = scipy.argmax(f > fmax) - 1
+    y = scipy.trapz(Pxx[ind_min: ind_max], f[ind_min: ind_max])
+    return y
+
+
+# def calc_bands_power(x, dt, bands):
+#     """
+#     From https://dsp.stackexchange.com/a/48210
+#     :param x: data array for one channel
+#     :param dt: sample frequency
+#     :param bands: dict of eeg_bands
+#     :return: dict of power values one for each band
+#     """
+#     f, psd = scipy.signal.welch(x, fs=1. / dt)
+#     power = {band: np.mean(psd[np.where((f >= lf) & (f <= hf))])
+#              for band, (lf, hf) in bands.items()}
+#     return power
 
 
 data_file = open('trimmed_emotiv_values_2019-02-05_18-32-30.371051.csv', 'r')
-data_arr = np.asarray([data_file.next().strip().split(',')], dtype=np.float64) - 4100  # normalize
+data_arr = np.asarray([data_file.next().strip().split(',')], dtype=np.float64)
 
 # print("now sending data...")
 
 for line in data_file:
 
-    packet_data = np.asarray(line.strip().split(','), dtype=np.float64) - 4100  # normalize
+    packet_data = np.asarray(line.strip().split(','), dtype=np.float64)
 
-    if len(data_arr) == 256:  # 64:
-        f3_data = data_arr[:, chans['F3']]
-        fc5_data = data_arr[:, chans['FC5']]
-        af3_data = data_arr[:, chans['AF3']]
-        f7_data = data_arr[:, chans['F7']]
-        t7_data = data_arr[:, chans['T7']]
-        p7_data = data_arr[:, chans['P7']]
+    if len(data_arr) == num_packets:
+
+        # Get Data for O1 and O2 channel
         o1_data = data_arr[:, chans['O1']]
-        # o1_data_filt = mne.filter.filter_data(o1_data, sample_freq, bp_low, bp_high)
         o2_data = data_arr[:, chans['O2']]
-        # o2_data_filt = mne.filter.filter_data(o2_data, sample_freq, bp_low, bp_high)
-        p8_data = data_arr[:, chans['P8']]
-        t8_data = data_arr[:, chans['T8']]
-        f8_data = data_arr[:, chans['F8']]
-        ac4_data = data_arr[:, chans['AC4']]
-        fc6_data = data_arr[:, chans['FC6']]
-        f4_data = data_arr[:, chans['F4']]
 
-        f3_power = calc_bands_power(f3_data, sample_freq, eeg_bands)
-        fc5_power = calc_bands_power(fc5_data, sample_freq, eeg_bands)
-        af3_power = calc_bands_power(af3_data, sample_freq, eeg_bands)
-        f7_power = calc_bands_power(f7_data, sample_freq, eeg_bands)
-        t7_power = calc_bands_power(t7_data, sample_freq, eeg_bands)
-        p7_power = calc_bands_power(p7_data, sample_freq, eeg_bands)
-        o1_power = calc_bands_power(o1_data, sample_freq, eeg_bands)
-        o2_power = calc_bands_power(o2_data, sample_freq, eeg_bands)
-        p8_power = calc_bands_power(p8_data, sample_freq, eeg_bands)
-        t8_power = calc_bands_power(t8_data, sample_freq, eeg_bands)
-        f8_power = calc_bands_power(f8_data, sample_freq, eeg_bands)
-        ac4_power = calc_bands_power(ac4_data, sample_freq, eeg_bands)
-        fc6_power = calc_bands_power(fc6_data, sample_freq, eeg_bands)
-        f4_power = calc_bands_power(f4_data, sample_freq, eeg_bands)
+        # Filtering
+        # o1_data_filt = butter_bandpass_filter(o1_data, bp_low, bp_high, sample_freq, order=5)
+        # o2_data_filt = butter_bandpass_filter(o2_data, bp_low, bp_high, sample_freq, order=5)
 
-        power_values_delta = [f3_power['Delta'], fc5_power['Delta'], af3_power['Delta'], f7_power['Delta'],
-                              t7_power['Delta'], p7_power['Delta'], o1_power['Delta'], o2_power['Delta'],
-                              p8_power['Delta'], t8_power['Delta'], f8_power['Delta'], ac4_power['Delta'],
-                              fc6_power['Delta'], f4_power['Delta']]
-        power_values_theta = [f3_power['Theta'], fc5_power['Theta'], af3_power['Theta'], f7_power['Theta'],
-                              t7_power['Theta'], p7_power['Theta'], o1_power['Theta'],
-                              o2_power['Theta'], p8_power['Theta'], t8_power['Theta'], f8_power['Theta'],
-                              ac4_power['Theta'], fc6_power['Theta'], f4_power['Theta']]
-        power_values_alpha = [f3_power['Alpha'], fc5_power['Alpha'], af3_power['Alpha'], f7_power['Alpha'],
-                              t7_power['Alpha'], p7_power['Alpha'], o1_power['Alpha'], o2_power['Alpha'],
-                              p8_power['Alpha'], t8_power['Alpha'], f8_power['Alpha'], ac4_power['Alpha'],
-                              fc6_power['Alpha'], f4_power['Alpha']]
-        power_values_beta = [f3_power['Beta'], fc5_power['Beta'], af3_power['Beta'], f7_power['Beta'], t7_power['Beta'],
-                             p7_power['Beta'], o1_power['Beta'], o2_power['Beta'], p8_power['Beta'], t8_power['Beta'],
-                             f8_power['Beta'], ac4_power['Beta'], fc6_power['Beta'], f4_power['Beta']]
-        power_values_gamma = [f3_power['Gamma'], fc5_power['Gamma'], af3_power['Gamma'], f7_power['Gamma'],
-                              t7_power['Gamma'], p7_power['Gamma'], o1_power['Gamma'], o2_power['Gamma'],
-                              p8_power['Gamma'], t8_power['Gamma'], f8_power['Gamma'], ac4_power['Gamma'],
-                              fc6_power['Gamma'], f4_power['Gamma']]
+        # Thresholding
+        # o1_amplitude = np.max(o1_data) - np.min(o1_data)
+        # o2_amplitude = np.max(o2_data) - np.min(o2_data)
 
-        data_arr = np.asarray([packet_data])
+        # Calculate Alpha Band Power for O1 and O2
+        fmin, fmax = eeg_bands['Alpha']
+        p_o1 = calc(o1_data, fmin, fmax)
+        p_o2 = calc(o2_data, fmin, fmax)
 
         print 'New Packet Buffer [64]:'
-        print 'O1 Alpha Power:', o1_power['Alpha']
-        print 'O2 Alpha Power:', o2_power['Alpha']
+        print 'O1 Alpha Power:', p_o1
+        print 'O2 Alpha Power:', p_o2
+
+        # Reset the data array
+        data_arr = np.asarray([packet_data])
 
         # now send it and wait for a bit
         # outlet.push_sample(power_values_delta)
         time.sleep(1.0 / sample_freq)
     else:
+        # keep collecting pakets until 64
         data_arr = np.append(data_arr, [packet_data], axis=0)
 
 print
