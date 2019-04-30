d09ecea34efd2aed836b7304bd7b0c130221fdbf
Corey Scheideman <corey9878@gmail.com>
2019-04-30 17:05:35 -0400

    convert test script to module

--- BB8test.py
+++ BB8test.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python
-# from bluepy import btle
-# import struct
-import time
-import BB8_driver
-import sys
-bb8 = BB8_driver.Sphero()
-bb8.connect()
-print 'connected'
 
-bb8.start()
-time.sleep(2)
-print 'sending set rgb led red'
-bb8.set_rgb_led(255, 0, 0, 0, False)
-time.sleep(1)
-print 'sending set rgb led green'
-bb8.set_rgb_led(0, 255, 0, 0, False)
-time.sleep(1)
-print 'sending set rgb led blue'
-bb8.set_rgb_led(0, 0, 255, 0, False)
-time.sleep(3)
-bb8.join()
-bb8.disconnect()
-print 'disconnected'
-sys.exit(1)
+def test_connection():
+    import time
+    import BB8_driver
+    import sys
+
+    bb8 = BB8_driver.Sphero()
+    bb8.connect()
+    print 'connected'
+
+    bb8.start()
+
+    print 'sending set rgb led red'
+    bb8.set_rgb_led(255, 0, 0, 0, False)
+    time.sleep(1)
+
+    print 'sending set rgb led green'
+    bb8.set_rgb_led(0, 255, 0, 0, False)
+    time.sleep(1)
+
+    print 'sending set rgb led blue'
+    bb8.set_rgb_led(0, 0, 255, 0, False)
+    time.sleep(3)
+
+    bb8.join()
+    bb8.disconnect()
+    print 'disconnected'
+
+    sys.exit(1)
+
+if __name__ == "__main__":
+    test_connection()
