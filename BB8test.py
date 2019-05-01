e1cadb3701aa991fa00d213f29796cfc10212b8d
Corey Scheideman <corey9878@gmail.com>
2019-05-01 16:11:23 -0400

    fix standalone test

--- BB8test.py
+++ BB8test.py
@@ -5,25 +5,23 @@ def test_connection():
 
     import time
     from bb8 import BB8
-    import dummy
-    dummy.bb.disconnect()
 
-    bb8 = BB8('F2:D8:37:4B:CE:F1')
+    bb = BB8('F2:D8:37:4B:CE:F1')
     print 'connected'
 
     print 'sending set rgb led red'
-    bb8.cmd(0x02, 0x20, [0xff, 0x0, 0x0, 0x0])
+    bb.cmd(0x02, 0x20, [0xff, 0x0, 0x0, 0x0])
     time.sleep(1)
 
     print 'sending set rgb led green'
-    bb8.cmd(0x02, 0x20, [0x00, 0xff, 0x0, 0x0])
+    bb.cmd(0x02, 0x20, [0x00, 0xff, 0x0, 0x0])
     time.sleep(1)
 
     print 'sending set rgb led blue'
-    bb8.cmd(0x02, 0x20, [0x0, 0x0, 0xff, 0x0])
+    bb.cmd(0x02, 0x20, [0x0, 0x0, 0xff, 0x0])
     time.sleep(3)
 
-    bb8 = bb8
+    bb.disconnect()
 
 
 if __name__ == "__main__":
