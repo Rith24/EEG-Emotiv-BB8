97f9e9ae83acd6d98176e02f096777ddde6b8bfb
Corey Scheideman <corey9878@gmail.com>
2019-05-01 15:15:36 -0400

    copy test_connection function from BB8test.py

--- training_GUI_support.py
+++ training_GUI_support.py
@@ -49,6 +49,33 @@ def processEntry():
     sys.stdout.flush()
 
 
+def test_connection():
+    import time
+    import BB8_driver
+
+    bb8 = BB8_driver.Sphero()
+    bb8.connect()
+    print 'connected'
+
+    bb8.start()
+
+    print 'sending set rgb led red'
+    bb8.set_rgb_led(255, 0, 0, 0, False)
+    time.sleep(1)
+
+    print 'sending set rgb led green'
+    bb8.set_rgb_led(0, 255, 0, 0, False)
+    time.sleep(1)
+
+    print 'sending set rgb led blue'
+    bb8.set_rgb_led(0, 0, 255, 0, False)
+    time.sleep(3)
+
+    bb8.join()
+    bb8.disconnect()
+    print 'disconnected'
+
+
 def return_to_main_program():
     global abt_trained
     global use_abt_trained
