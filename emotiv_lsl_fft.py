4743025be2dd1406605ac284d81ed9cf0cc3230a
Corey Scheideman <corey9878@gmail.com>
2019-06-11 11:53:44 -0400

    mod for 1 min timed 3x2 trials main recordings

--- emotiv_lsl_fft.py
+++ emotiv_lsl_fft.py
@@ -157,135 +157,146 @@ def main():
     # abt_trained = ((avg_o1_alpha_eyesopen/avg_o1_theta_eyesopen) + (avg_o1_alpha_eyesclosed/avg_o1_theta_eyesclosed))/2
     # abt_trained = training.get_average_abt()
 
-    raw_input('Training Complete. Press Enter to continue...')
+    print 'Training Complete.'
 
-    data_arr = []
-    inst_color = None
-    inst_direction = None
-    with Emotiv(display_output=False, write=True, verbose=True) as headset:
-        while True:
-            try:
-                packet = headset.dequeue()
-                if packet is not None:
-                    # Get the 14 values from emotiv packet
-                    sample = [headset.sensors[c]['value'] for c in ch_names]
+    for t in range(6):
+        if t % 2 == 0:
+            print 'Record Eyes Open'
+        else:
+            print 'Record Eyes Closed'
+        raw_input('Press Enter to continue...')
+        start_time = time.time()
+        end_time = time.time()
 
-                    data_arr.append(sample)
+        data_arr = []
+        inst_color = None
+        inst_direction = None
+        with Emotiv(display_output=False, write=True, verbose=True) as headset:
+            # while True:
+            while end_time - start_time < 60:
+                try:
+                    packet = headset.dequeue()
+                    if packet is not None:
+                        # Get the 14 values from emotiv packet
+                        sample = [headset.sensors[c]['value'] for c in ch_names]
 
-                    if len(data_arr) == num_packets:
+                        data_arr.append(sample)
 
-                        # Get Data for O1 and O2 channel
-                        o1_data = [col[chans['O1']] for col in data_arr]
-                        o2_data = [col[chans['O2']] for col in data_arr]
+                        if len(data_arr) == num_packets:
 
-                        if len(o1_data) == len(o2_data):
-                            for i in range(len(o1_data)):
-                                o1_data[i] = o1_data[i] - 4300
-                                o2_data[i] = o2_data[i] - 4300
+                            # Get Data for O1 and O2 channel
+                            o1_data = [col[chans['O1']] for col in data_arr]
+                            o2_data = [col[chans['O2']] for col in data_arr]
 
-                        # Filtering
-                        o1_data_filt = butter_bandpass_filter(
-                            o1_data, bp_low, bp_high, sample_freq, order=5)
-                        o2_data_filt = butter_bandpass_filter(
-                            o2_data, bp_low, bp_high, sample_freq, order=5)
+                            if len(o1_data) == len(o2_data):
+                                for i in range(len(o1_data)):
+                                    o1_data[i] = o1_data[i] - 4300
+                                    o2_data[i] = o2_data[i] - 4300
 
-                        # Thresholding
-                        o1_amplitude = max(o1_data) - min(o1_data)
-                        o2_amplitude = max(o2_data) - min(o2_data)
+                            # Filtering
+                            o1_data_filt = butter_bandpass_filter(
+                                o1_data, bp_low, bp_high, sample_freq, order=5)
+                            o2_data_filt = butter_bandpass_filter(
+                                o2_data, bp_low, bp_high, sample_freq, order=5)
 
-                        # Calculate Alpha Band Power
-                        fmin, fmax = eeg_bands['Alpha']
-                        ap_o1 = calc(o1_data_filt, fmin, fmax)
-                        ap_o2 = calc(o2_data_filt, fmin, fmax)
+                            # Thresholding
+                            o1_amplitude = max(o1_data) - min(o1_data)
+                            o2_amplitude = max(o2_data) - min(o2_data)
 
-                        # Calculate Theta Band Power
-                        fmin, fmax = eeg_bands['Theta']
-                        tp_o1 = calc(o1_data_filt, fmin, fmax)
-                        tp_o2 = calc(o2_data_filt, fmin, fmax)
+                            # Calculate Alpha Band Power
+                            fmin, fmax = eeg_bands['Alpha']
+                            ap_o1 = calc(o1_data_filt, fmin, fmax)
+                            ap_o2 = calc(o2_data_filt, fmin, fmax)
 
-                        # Calculate Alpha / Theta
-                        abt_o1 = ap_o1 / tp_o1
-                        abt_o2 = ap_o2 / tp_o2
-                        abt_avg = (abt_o1 + abt_o2) / 2
+                            # Calculate Theta Band Power
+                            fmin, fmax = eeg_bands['Theta']
+                            tp_o1 = calc(o1_data_filt, fmin, fmax)
+                            tp_o2 = calc(o2_data_filt, fmin, fmax)
 
-                        print '#' * 80
-                        print 'O1 Alpha:', ap_o1, '|', 'O1 Theta:', tp_o1
-                        print 'O1 Alpha/Theta:', abt_o1
-                        print 'O2 Alpha:', ap_o2, '|', 'O2 Theta:', tp_o2
-                        print 'O2 Alpha/Theta:', abt_o2
-                        print 'Avg Alpha/Theta:', abt_avg, '|', 'Trained Avg Alpha/Theta:', abt_trained
+                            # Calculate Alpha / Theta
+                            abt_o1 = ap_o1 / tp_o1
+                            abt_o2 = ap_o2 / tp_o2
+                            abt_avg = (abt_o1 + abt_o2) / 2
 
-                        if not use_abt_trained:
-                            # or abs(o2_amplitude > max_amplitude):
-                            if abs(o1_amplitude) > max_amplitude:
-                                print '-*- o1_amp:', o1_amplitude, '|', 'o2_amp:', o2_amplitude
-                                print 'color(yellow)'
-                                # color(yellow)
-                                inst_color = yellow
-                                inst_direction = None
-                            # might change to < if value decreases in each successive window (meditative state)
-                            elif abt_avg > previous_avg:
-                                print 'color(purple)'
-                                # color(purple)
-                                inst_color = purple
-                                print 'roll(False)'
-                                # roll(False)
-                                inst_direction = False
+                            print '#' * 80
+                            print 'O1 Alpha:', ap_o1, '|', 'O1 Theta:', tp_o1
+                            print 'O1 Alpha/Theta:', abt_o1
+                            print 'O2 Alpha:', ap_o2, '|', 'O2 Theta:', tp_o2
+                            print 'O2 Alpha/Theta:', abt_o2
+                            print 'Avg Alpha/Theta:', abt_avg, '|', 'Trained Avg Alpha/Theta:', abt_trained
+
+                            if not use_abt_trained:
+                                # or abs(o2_amplitude > max_amplitude):
+                                if abs(o1_amplitude) > max_amplitude:
+                                    print '-*- o1_amp:', o1_amplitude, '|', 'o2_amp:', o2_amplitude
+                                    print 'color(yellow)'
+                                    # color(yellow)
+                                    inst_color = yellow
+                                    inst_direction = None
+                                # might change to < if value decreases in each successive window (meditative state)
+                                elif abt_avg > previous_avg:
+                                    print 'color(purple)'
+                                    # color(purple)
+                                    inst_color = purple
+                                    print 'roll(False)'
+                                    # roll(False)
+                                    inst_direction = False
+                                else:
+                                    print 'color(green)'
+                                    # color(green)
+                                    inst_color = green
+                                    print 'roll(True)'
+                                    # roll(True)
+                                    inst_direction = True
+
+                                previous_avg = abt_avg
+
                             else:
-                                print 'color(green)'
-                                # color(green)
-                                inst_color = green
-                                print 'roll(True)'
-                                # roll(True)
-                                inst_direction = True
+                                # or abs(o2_amplitude > max_amplitude):
+                                if abs(o1_amplitude) > max_amplitude:
+                                    print '-*- o1_amp:', o1_amplitude, '|', 'o2_amp:', o2_amplitude
+                                    print 'color(yellow)'
+                                    # color(yellow)
+                                    inst_color = yellow
+                                    inst_direction = None
+                                elif abt_avg > abt_trained:
+                                    print 'color(purple)'
+                                    # color(purple)
+                                    inst_color = purple
+                                    print 'roll(False)'
+                                    # roll(False)
+                                    inst_direction = False
+                                else:
+                                    print 'color(green)'
+                                    # color(green)
+                                    inst_color = green
+                                    print 'roll(True)'
+                                    # roll(True)
+                                    inst_direction = True
 
-                            previous_avg = abt_avg
+                            data_arr = []
+                            # now send it and wait for a bit
+                            # outlet.push_sample(power_values_delta)
+                            time.sleep(1.0 / sample_freq)
 
-                        else:
-                            # or abs(o2_amplitude > max_amplitude):
-                            if abs(o1_amplitude) > max_amplitude:
-                                print '-*- o1_amp:', o1_amplitude, '|', 'o2_amp:', o2_amplitude
-                                print 'color(yellow)'
-                                # color(yellow)
-                                inst_color = yellow
-                                inst_direction = None
-                            elif abt_avg > abt_trained:
-                                print 'color(purple)'
-                                # color(purple)
-                                inst_color = purple
-                                print 'roll(False)'
-                                # roll(False)
-                                inst_direction = False
-                            else:
-                                print 'color(green)'
-                                # color(green)
-                                inst_color = green
-                                print 'roll(True)'
-                                # roll(True)
-                                inst_direction = True
+                        # constant rolling, only updated when new packet group is calculated
+                        # every 16th packet, to give time for rolling
+                        if (len(data_arr) + 1) % 16 == 0:
+                            if inst_color is not None:
+                                print "color(%s)" % inst_color
+                                color(inst_color)
+                            if inst_direction is not None:
+                                print "roll(%s)" % inst_direction
+                                roll(inst_direction)
+                    # else:
+                        # break
+                    end_time = time.time()
+                except KeyboardInterrupt:
+                    break
 
-                        data_arr = []
-                        # now send it and wait for a bit
-                        # outlet.push_sample(power_values_delta)
-                        time.sleep(1.0 / sample_freq)
-
-                    # constant rolling, only updated when new packet group is calculated
-                    # every 16th packet, to give time for rolling
-                    if (len(data_arr) + 1) % 16 == 0:
-                        if inst_color is not None:
-                            print "color(%s)" % inst_color
-                            color(inst_color)
-                        if inst_direction is not None:
-                            print "roll(%s)" % inst_direction
-                            roll(inst_direction)
-                # else:
-                    # break
-            except KeyboardInterrupt:
-                break
-
-        bb.disconnect()
-        print "Done."
-        sys.exit(0)
+    bb.disconnect()
+    print "Done."
+    sys.exit(0)
 
 
 if __name__ == "__main__":
